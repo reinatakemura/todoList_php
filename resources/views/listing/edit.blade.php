22b87681ace0caa2721c48909de8b063a67d2c8b
reinatakemura <sleepfreemusic.tb@gmail.com>
2020-06-09 14:38:53 +0900

    Create listing/edit.blade.php

--- resources/views/listing/edit.blade.php
+++ resources/views/listing/edit.blade.php
@@ -5,17 +5,20 @@
   <!-- バリデーションエラーの場合に表示 -->
   @include('common.errors')
     <!-- リスト作成フォーム -->
-    <form action="{{ url('listings') }}" method="post" class="form-horizontal">
+    <form action="{{ url('listing/edit') }}" method="post" class="form-horizontal">
     {{ csrf_field() }}
       <div class="form-group">
         <label for="listing" class="col-sm-3 control-label">リスト名</label>
         <div class="col-sm-6">
-          <input type="text" name="list_name" class="form-control" value="{{ old('list_name') }}">
+          <input type="text" name="list_name" class="form-control" value="{{ old('list_name', $listing->title) }}">
         </div>
+        <input type="hidden" name="id" value="{{ old('id', $listing->id) }}">
       </div>
       <div class="form-group">
         <div class="col-sm-offset-3 col-sm-6">
-          <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-plus"></i>作成</button>
+          <button type="submit" class="btn btn-default">
+            <i class="glyphicon glyphicon-saved"></i>更新
+          </button>
         </div>
       </div>
     </form>
